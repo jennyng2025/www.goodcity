c1acf902383f7984d0839b87711ed192a1c228d6
Adam George <finechocolate@gmail.com>
2014-07-24 10:33:20 +1000

    Fix for iPad, iPad mini. Removed full-screen height layout for tablets. Removed orientation change reload javascript. Added more flexibility for display or CR logo. Updated stories page.

--- js/min/app-ck.js
+++ js/min/app-ck.js
@@ -1,1 +1,1 @@
-$(document).foundation(),$(document).ready(function(){matchMedia(Foundation.media_queries.small).matches&&$(document).scroll(function(){$(document).scrollTop()>=150?$("#top-link a").show():$("#top-link a").hide()}),window.onorientationchange=function(){window.location.reload()}});
+$(document).foundation(),$(document).ready(function(){matchMedia(Foundation.media_queries.small).matches&&$(document).scroll(function(){$(document).scrollTop()>=150?$("#top-link a").show():$("#top-link a").hide()})});
