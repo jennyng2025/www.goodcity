2b6fb1516efb6581b75bd5d05d716611fda666a2
Steve Kenworthy <steveyken@gmail.com>
2015-09-29 17:33:21 +0800

    Use http for git

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,6 @@
 lock '3.4.0'
 
 set :application, 'www.goodcity.hk'
-set :repo_url, 'git@github.com:crossroads/www.goodcity.git'
+set :repo_url, 'https://github.com/crossroads/www.goodcity.git'
 
 set :deploy_to, '/var/www/html/www.goodcity.hk'
