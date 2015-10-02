3645fabc7c42d08628dd1690f56e6dbeef4bccd7
Steve Kenworthy <steveyken@gmail.com>
2015-10-02 14:33:54 +0800

    Fixed live deployments

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server 'goodcity-production.cloudapp.net', user: 'deployer', roles: %w{web}, primary: true
+server 'www.goodcity.hk', user: 'deployer', roles: %w{web}, primary: true
 server 'goodcity-production.cloudapp.net:62423', user: 'deployer', roles: %w{web}
 set :branch, :live
